2a7903d7103a44f00eaa003e6bd2c3c250eb478b
George Chacko <chackoge@gmail.com>
2020-05-31 18:30:53 -0400

    minor tweak

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,7 +2,7 @@
 #
 # Exercise 1.5
 init_ht=100
-print('iteration','value',sep="   ")
+print('iteration','value',sep="\t")
 for i in range(10):
     init_ht *= 0.6
     print(i+1,round(init_ht,4),sep="\t"*3)
