e0e5f83200b6950a86fb3441850e31acf4cdf3ea
George Chacko <chackoge@gmail.com>
2020-05-31 17:25:22 -0400

    initial commit

--- Work/bounce.py
+++ Work/bounce.py
@@ -2,7 +2,8 @@
 #
 # Exercise 1.5
 init_ht=100
+print('iteration','value',sep="   ")
 for i in range(10):
     ht=init_ht * 0.6
     init_ht=ht
-    print(i+1,round(ht,4))
+    print(i+1,round(ht,4),sep="\t\t\t")
